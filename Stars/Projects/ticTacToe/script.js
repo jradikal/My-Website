0d4e6a5d88de72165fbc4ef581dcda44386b32d3
Jared <jaredjayz@hotmail.com>
2018-06-22 10:08:43 +0100

    added user behavior to ticTacToe

--- Stars/Projects/ticTacToe/script.js
+++ Stars/Projects/ticTacToe/script.js
@@ -11,3 +11,26 @@ const winCombos = [
     [0, 4, 8],
     [6, 4, 2],
 ]
+
+const cells = document.querySelectorAll('.cell');
+
+startGame();
+
+function startGame() {
+    document.querySelector(".endgame").style.display = "none";
+    origBoard = Array.from(Array(9).keys());
+    for (var i = 0; i < cells.length; i++) {
+        cells[i].innerText = '';
+        cells[i].style.removeProperty('background-color');
+        cells[i].addEventListener('click', turnClick, false);
+    }
+}
+
+function turnClick(square) {
+    turn(square.target.id, huPlayer);
+}
+
+function turn(squareId, player) {
+    origBoard[squareId] = player;
+    document.getElementById(squareId).innerText = player;
+}
